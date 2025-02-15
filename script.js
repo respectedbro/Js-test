b1c833a97d3b10e71870c93b4901bfe21c4082c3
NikDm <respectedoff@yandex.ru>
2025-02-15 14:39:18 +0300

    starts

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "title";
+const screens = "screens";
+const screenPrice = " screenPrice";
+const rollback = " rollback";
+const fullPrice = " fullPrice";
+const adaptive = " adaptive";
+
+alert("Hello");
+console.log("Hello");
