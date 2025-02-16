f9ca3fba227357a39e1ecadcc1b802fbbd395778
resAsus <respectedbro@yandex.ru>
2025-02-16 17:02:25 +0300

    types

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const title = "title";
-const screens = "screens";
-const screenPrice = " screenPrice";
-const rollback = " rollback";
-const fullPrice = " fullPrice";
-const adaptive = " adaptive";
+"use strict"
 
-alert("Hello");
-console.log("Hello");
+const title = 'Проект'
+let screens = 'Простые, Сложные, Интерактивные'
+const screenPrice = 500
+const rollback = 80
+let fullPrice = 5000
+const adaptive = false
+
+
+console.log(typeof title)
+console.log(screens.length)
+console.log(`“Стоимость
+верстки
+экранов ${screenPrice}
+рублей / долларов / гривен / юани” и Стоимость
+разработки
+сайта ${fullPrice}
+рублей / долларов / гривен / юани”`)
+screens.toLowerCase()
+
+screens = screens.split( ' ')
+console.log(screens)
+
+fullPrice = fullPrice * (rollback/100)
+console.log(fullPrice)
